a0eef11fb5a0bf4dbcefa546b7e2db344ae03f24
pingkang <pingkang@jinher.com>
2021-08-27 15:52:19 +0800

    no message

--- Sources/JHBase/JHBaseDateExt.swift
+++ Sources/JHBase/JHBaseDateExt.swift
@@ -1,8 +1,38 @@
 //
-//  File.swift
+//  JHBaseDateExt.swift
 //  
 //
 //  Created by pk on 2021/8/27.
 //
 
 import Foundation
+
+@objc extension NSDate{
+    
+    public static func timestampSince1970() -> String {
+        let datenow = NSDate()
+        //现在时间
+        let interval = datenow.timeIntervalSince1970*1000
+        let timeSp = String(format: "%.0f", interval)
+        return timeSp
+    }
+    
+    public static func changeTimeStampToString(timeStamp:String) -> String {
+        var fStr = "yyyy-MM-dd HH:mm:ss"
+        if timeStamp.contains(".") {
+            fStr = "yyyy.MM.dd HH:mm:ss"
+        } else if timeStamp.contains("年") {
+            fStr = "yyyy年MM月dd日 HH:mm:ss"
+        }
+        let formatter = DateFormatter()
+        formatter.dateFormat = fStr
+        let dateCurrentStr = formatter.date(from: timeStamp)
+        if (dateCurrentStr == nil) {
+            //格式不对是显示当前时间
+            return "\(CLong(NSDate().timeIntervalSince1970))"
+        }
+        let timeSp = "\(CLong(dateCurrentStr!.timeIntervalSince1970))"
+        return timeSp
+    }
+    
+}
--- Sources/JHBase/JHBaseStringExt.swift
+++ Sources/JHBase/JHBaseStringExt.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+@objc extension NSString{
+    
+    func isEmpty() -> Bool {
+        if type(of: self).isEqual(NSNull.self) {
+            return true
+        }
+        if self.trimmingCharacters(in: NSCharacterSet.whitespaces).count == 0 {
+            return true
+        }
+        if self.isEqual(to: "(null)") {
+            return true
+        }
+        if self.isEqual(to: "<null>") {
+            return true
+        }
+        return false
+    }
+    
+    static func contentIsNullORNil(content:NSString) -> Bool{
+        if type(of: content).isEqual(NSNull.self) {
+            return true
+        }
+        if content.trimmingCharacters(in: NSCharacterSet.whitespaces).count == 0 {
+            return true
+        }
+        if content.isEqual(to: "(null)") {
+            return true
+        }
+        if content.isEqual(to: "<null>") {
+            return true
+        }
+        return false
+    }
+}
+
+
