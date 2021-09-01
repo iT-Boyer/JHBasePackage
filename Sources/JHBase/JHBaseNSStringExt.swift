099114bf62ee6b1666e5eb999281df182100094f
lifei <lifei_zdjl@126.com>
2021-09-01 19:24:38 +0800

    string.

---
+++ Sources/JHBase/JHBaseNSStringExt.swift
@@ -0,0 +1,39 @@
+//
+//  File.swift
+//  
+//
+//  Created by pk on 2021/8/27.
+//  NSString 便利方法
+
+import Foundation
+
+@objc extension NSString{
+    
+    public func isEmptyOrNull() -> Bool {
+        if self.trimmingCharacters(in: NSCharacterSet.whitespaces).isEmpty {
+            return true
+        }
+        if self.isEqual(to: "(null)") {
+            return true
+        }
+        if self.isEqual(to: "<null>") {
+            return true
+        }
+        return false
+    }
+    
+    public static func isEmptyOrNull(_ content: NSString) -> Bool{
+        if content.trimmingCharacters(in: NSCharacterSet.whitespaces).isEmpty {
+            return true
+        }
+        if content.isEqual(to: "(null)") {
+            return true
+        }
+        if content.isEqual(to: "<null>") {
+            return true
+        }
+        return false
+    }
+}
+
+
--- Sources/JHBase/JHBaseStringExt.swift
+++ Sources/JHBase/JHBaseStringExt.swift
@@ -2,44 +2,30 @@
 //  File.swift
 //  
 //
-//  Created by pk on 2021/8/27.
-//  NSString 便利方法
+//  Created by lifei on 2021/9/1.
+//  String 便利方法
 
 import Foundation
 
-@objc extension NSString{
-    
-    public func isEmptyOrNull() -> Bool {
-        if self is NSNull {
-            return true
-        }
-        if self.trimmingCharacters(in: NSCharacterSet.whitespaces).isEmpty {
-            return true
-        }
-        if self.isEqual(to: "(null)") {
-            return true
-        }
-        if self.isEqual(to: "<null>") {
+extension String {
+    public var boolValue: Bool {
+        let strLower = self.lowercased()
+        let strInt = Int(self) ?? 0
+        if strLower == "true" || strLower == "yes" || strInt > 0{
             return true
         }
         return false
     }
     
-    public static func isEmptyOrNull(_ content: NSString) -> Bool{
-        if content is NSNull {
-            return true
-        }
-        if content.trimmingCharacters(in: NSCharacterSet.whitespaces).isEmpty {
-            return true
-        }
-        if content.isEqual(to: "(null)") {
-            return true
-        }
-        if content.isEqual(to: "<null>") {
-            return true
-        }
-        return false
+    public var doubleValue: Double {
+        return Double(self) ?? 0
+    }
+    
+    public var floatValue: Float {
+        return Float(self) ?? 0
+    }
+    
+    public var intValue: Int {
+        return Int(self) ?? 0
     }
 }
-
-
