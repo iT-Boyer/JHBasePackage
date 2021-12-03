4856e13afc8da8ccba22d125532e53a8516ea02b
lifei <lifei_zdjl@126.com>
2021-12-03 15:42:35 +0800

    私有化部署需求，封装域名统一读取工具

--- JHBase/Extension/JHBaseURLExt.swift
+++ JHBase/Extension/JHBaseURLExt.swift
@@ -9,36 +9,10 @@ import Foundation
 
 extension URL {
     public static func changProtocolURL(with urlString: String?) -> URL? {
-        guard let urlStr = urlString, urlStr.isEmpty == false else { return nil }
-        
-        let lowerUrl = urlStr.lowercased()
-        if lowerUrl.hasPrefix("https") {
-            return URL(string: urlStr)
+        let httpStr = JHBaseURL.changeProtocol(with: urlString)
+        if httpStr.isEmpty == true {
+            return nil
         }
-        
-        let whiteList = ["http://testlvp.iuoooo.com/H5MyLive/Index",
-                         "http://lvp.iuoooo.com/H5MyLive/Index",
-                         "http://open.ys7.com/openlive",
-                         "http://www.iermu.com/thirdpart_shareid.php",
-                         "http://d.pcs.baidu.com",
-                         "lvp.iuoooo.com",
-                         "iwp.e.iuoooo",
-                         "bbs.iuoooo.com"]
-        for whiteStr in whiteList {
-            if urlStr.contains(whiteStr) == true {
-                return URL(string: urlStr)
-            }
-        }
-        
-        if lowerUrl.hasPrefix("http") == false || lowerUrl.contains("iuoooo.com") == false {
-            return URL(string: urlStr)
-        }
-        // 不包含 iuoooo.com
-        var urlList:[String] = urlStr.components(separatedBy: "://")
-        if urlList.count > 0 {
-            urlList[0] = "https"
-        }
-        let joinedUrl = urlList.joined(separator: "://")
-        return URL(string: joinedUrl)
+        return URL(string: httpStr)
     }
 }
---
+++ JHBase/Utils/JHBaseURL.swift
@@ -0,0 +1,98 @@
+//  Created by lifei on 2021/11/29.
+//  域名读取工具
+
+import Foundation
+
+public class JHBaseURL: NSObject {
+    public static let shared = JHBaseURL()
+    private override init() {}
+    
+    // MARK: - API
+    public static let netProtocol: String = shared.netProtocol
+    public static let environment: String = shared.environment
+    public static let uploadURL: String = shared.uploadBaseURL
+    public static let downloadURL: String = shared.downloadBaseURL
+    
+    public static func domain(for key: String?) -> String {
+        //TODO: - 待增加私有化逻辑
+        guard let domainKey = key, domainKey.isEmpty == false else { return "" }
+        let domain = shared.ipFileDict[domainKey.lowercased()] ?? ""
+        return domain
+    }
+    
+    public static func fullURL(with key: String?, path: String?) -> String {
+        let domainKey = key ?? ""
+        let domainURL = domain(for: domainKey)
+        let domainPath = path ?? ""
+        return "\(netProtocol)\(environment)\(domainURL)\(domainPath)"
+    }
+    
+    public static func changeProtocol(with url: String?) -> String {
+        guard let urlStr = url, urlStr.isEmpty == false else { return "" }
+        
+        let lowerUrl = urlStr.lowercased()
+        if lowerUrl.hasPrefix("https") {
+            return urlStr
+        }
+        let isWhite = shared.whiteList.contains { urlStr.contains($0) }
+        if isWhite == true {
+            return urlStr
+        }
+        if lowerUrl.hasPrefix("http") == false || lowerUrl.contains("iuoooo.com") == false {
+            return urlStr
+        }
+        var urlList: [String] = urlStr.components(separatedBy: "://")
+        if urlList.count > 0 {
+            urlList[0] = "https"
+        }
+        let joinedUrl = urlList.joined(separator: "://")
+        return joinedUrl
+    }
+    
+    // MARK: - Lazy Load
+    private lazy var ipFileDict: [String: String] = {
+        let fileURL = Bundle.main.url(forResource: "ipFile", withExtension: "plist")
+        guard let tmpFileURL = fileURL else { return [:] }
+        let fileDict = try? NSDictionary(contentsOf: tmpFileURL, error: ())
+        if let fileDict = fileDict as? [String: String] {
+            return fileDict
+        }
+        return [:]
+    }()
+    
+    private lazy var environment: String = {
+        return JHBaseURL.domain(for: "api_environment")
+    }()
+    
+    private lazy var netProtocol: String = {
+        let urlProtocol = JHBaseURL.domain(for: "api_header")
+        if urlProtocol.isEmpty == true { return "https://" }
+        return urlProtocol
+    }()
+    
+    private lazy var uploadBaseURL: String = {
+        let domainTest = JHBaseURL.domain(for: "api_host_upload")
+        let domainProduct = JHBaseURL.domain(for: "api_host_upload_up")
+        if environment.isEmpty == false {
+            return "\(netProtocol)\(environment)\(domainTest)"
+        }
+        return "\(netProtocol)\(environment)\(domainProduct)"
+    }()
+    
+    private lazy var downloadBaseURL: String = {
+        let domain = JHBaseURL.domain(for: "api_host_upload")
+        return "\(netProtocol)\(environment)\(domain)"
+    }()
+    
+    private lazy var whiteList: [String] = {
+        let whiteList = ["http://testlvp.iuoooo.com/H5MyLive/Index",
+                         "http://lvp.iuoooo.com/H5MyLive/Index",
+                         "http://open.ys7.com/openlive",
+                         "http://www.iermu.com/thirdpart_shareid.php",
+                         "http://d.pcs.baidu.com",
+                         "lvp.iuoooo.com",
+                         "iwp.e.iuoooo",
+                         "bbs.iuoooo.com"]
+        return whiteList
+    }()
+}
